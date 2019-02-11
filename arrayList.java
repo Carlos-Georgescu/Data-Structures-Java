1a595094153d70deb425b0ecd4a2a644861a11ca
summers wu <wlysummers@gmail.com>
2019-02-10 21:50:33 -0500

    added some basic arrayList functions for syntax familiarization

---
+++ arrayList.java
@@ -0,0 +1,27 @@
+/*
+	Java Syntax Cheat Sheet #One
+	ArrayList
+*/
+
+ArrayList<Type> name = new ArrayList<Type>();//initialization with add
+
+Character, String, Map, Character, Integer, Boolean//available types
+
+name.add(value);//add a value to an ArrayList
+
+ArrayList<Type> name = new ArrayList<Type>( //initialization with as list
+            Arrays.asList(value, 
+                          value, 
+                          value));
+
+
+List<Integer> arr = new ArrayList<>(); //initialization with add
+        arr.add(1); 
+        arr.add(2); 
+        arr.add(3); 
+        arr.add(4); 
+        arr.add(5);
+
+List<Integer> newList = new ArrayList<Integer>(arr); //initialization with another ArrayList
+
+
